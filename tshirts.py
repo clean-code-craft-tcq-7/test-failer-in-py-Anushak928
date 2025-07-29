068931b57cf2c148a8574196410a36be085c556a
Anushak928 <73244898+Anushak928@users.noreply.github.com>
2025-07-29 10:01:00 +0530

    added more test cases

--- tshirts.py
+++ tshirts.py
@@ -1,8 +1,12 @@
+INVALID_INPUT = 'Invalid input'
+
 
 def size(cms):
+    if not isinstance(cms, (int, float)) or cms < 0:
+        return INVALID_INPUT
     if cms < 38:
         return 'S'
-    elif cms > 38 and cms < 42:
+    elif cms >= 38 and cms < 42:
         return 'M'
     else:
         return 'L'
@@ -11,4 +15,11 @@ def size(cms):
 assert(size(37) == 'S')
 assert(size(40) == 'M')
 assert(size(43) == 'L')
+assert(size(38)== 'M')
+assert(size(0) == 'S')
+assert(size(38) == 'M')
+assert(size(42) == 'L')
+assert(size(-1) == INVALID_INPUT)
+assert(size('abc') == INVALID_INPUT)
+assert(size(None) == INVALID_INPUT)
 print("All is well (maybe!)")
