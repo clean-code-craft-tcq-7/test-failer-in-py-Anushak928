3d587b4a7f2b96adb5b1e501d701f5211e6e4c41
Anushak928 <73244898+Anushak928@users.noreply.github.com>
2025-07-29 10:10:55 +0530

    variable name has been avoided

--- misaligned.py
+++ misaligned.py
@@ -1,12 +1,23 @@
-
-def print_color_map():
+def generate_color_map():
     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
-    for i, major in enumerate(major_colors):
-        for j, minor in enumerate(minor_colors):
-            print(f'{i * 5 + j} | {major} | {minor}')
-    return len(major_colors) * len(minor_colors)
+    return [(i * 5 + j + 1, major, minor) for i, major in enumerate(major_colors) for j, minor in enumerate(minor_colors)]
 
+
+def print_color_map():
+    color_map_for_print = generate_color_map()
+    print(f'{"No":<3} | {"Major":<6} | {"Minor":<6}')
+    print('-' * 25)
+    for num, major, minor in color_map_for_print:
+        print(f'{num:<3} | {major:<6} | {minor:<6}')
+    return len(color_map_for_print)
+
+
+# Simple tests
+color_map_for_test = generate_color_map()
+assert(len(color_map_for_test) == 25)
+assert(color_map_for_test[0] == (1, "White", "Blue"))
+assert(color_map_for_test[-1] == (25, "Violet", "Slate"))
 
 result = print_color_map()
 assert(result == 25)
